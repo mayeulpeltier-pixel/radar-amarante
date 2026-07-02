0c542b0898a0813cd7c786e3c13ae2abbebb2993
mayeulpeltier-pixel <mayeul.peltier@gmail.com>
2026-07-02 22:45:18 +0200

    Update suivi_config.py

--- suivi_config.py
+++ suivi_config.py
@@ -9,7 +9,7 @@
 
 # 1) URL de l'application web Apps Script (celle qui finit par /exec),
 #    obtenue à l'étape « Déployer » du script.
-SUIVI_WEBAPP_URL = ""
+SUIVI_WEBAPP_URL = "https://script.google.com/macros/s/AKfycby_UBZQxtyrNhiFG-OgPUE9kxyv8SyOKGkwqHpKxDWDTyGFDR6EiPi14HPSd-HUQxT7/exec"
 
 # 2) Le MÊME mot de passe que la variable TOKEN dans le script Apps Script.
-SUIVI_TOKEN = ""
+SUIVI_TOKEN = "Amarante2026"
